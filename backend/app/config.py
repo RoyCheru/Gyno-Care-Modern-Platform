f64efd2d0f496a9c6c99f6c80434760e9edadba1
RoyCheruiyot <roycheruiyot168@gmail.com>
2026-02-12 11:52:37 +0300

    switched to postgres

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey123")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///gynocare.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:roy12345@localhost:5432/gynocare")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "your-jwt-secret-key"
